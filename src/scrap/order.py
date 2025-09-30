c94436a66df6f223abafbbe872d79389bda73e46
JaumeFigueras <jaume.figueras@upc.edu>
2025-09-30 18:58:19 +0200

    added slots and comparison functions

--- src/scrap/order.py
+++ src/scrap/order.py
@@ -12,7 +12,21 @@ class ScrapOrderParams(TypedDict):
 
 
 class ScrapOrder:
+    __slots__ = ("scheduled_at", "stop_id")
 
     def __init__(self, **kwargs: Unpack[ScrapOrderParams]):
         self.scheduled_at = kwargs.get("scheduled_at")
         self.stop_id = kwargs.get("stop_id")
+
+
+    def __eq__(self, other):
+        if not isinstance(other, ScrapOrder):
+            return NotImplemented
+        return (self.scheduled_at, self.stop_id) == (other.scheduled_at, other.stop_id)
+
+    def __lt__(self, other):
+        if not isinstance(other, ScrapOrder):
+            return NotImplemented
+        if self.scheduled_at != other.scheduled_at:
+            return self.scheduled_at < other.scheduled_at
+        return self.stop_id < other.stop_id
